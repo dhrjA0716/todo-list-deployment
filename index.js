04f3d1eac6e6c130c9edcfd75d302e56215e3d62
Dheeraj <dhrja0716@gmail.com>
2025-09-21 15:05:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-const mongoose = require("mongoose");
-
 const uri = process.env.MONGO_URI;  /
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
